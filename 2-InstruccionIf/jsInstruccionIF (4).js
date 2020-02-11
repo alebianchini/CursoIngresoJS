a694db6a9fdee739e9fdb75e378db2fafd5b724d
Your Name <alejobianchini@hotmail.com>
2020-02-11 20:30:28 -0300

    asd

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    edad = parseInt(document.getElementById("edad").value);
+//si la edad esta dentro del rango
+ /*   if(edad >= 13 && edad < 18){
+        alert ("Usted es un adolescente");
+}*/
+
+//si(if) la edad no(not) esta fuera del rango
+    if( !( edad < 13 || edad > 17)) {
+        alert("Es adolescente");
+    }
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    edad = parseInt(document.getElementById("edad").value);
+//La manera más sencilla sería esta, utilizando las preguntas if más sencillas, que son adulto y niño
+    if(edad > 17){
+        alert("Es adulto");
+    }
+    else if(edad < 13) {
+        alert("Es niño");
+    }
+    else{
+        alert("Es adolescente");
+    }
 
-
-
-
+//pero de esta manera lo hice yo en un principio, también funciona
+/*    if(edad >= 18){
+        alert("Es adulto");
+    }
+    else if(edad >= 13 && edad < 18) {
+        alert("Es adolescente");
+    }
+    else{
+        alert("Es niño");
+    } */
 }//FIN DE LA FUNCIÓN
