6ea2972aab2d2a6d2cbf475572f6622102545c03
alebianchini <alejobianchini@hotmail.com>
2020-02-18 23:40:18 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,6 +3,7 @@ function mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+// while (  !(sexo == "f" || sexo == "m")  ) También se puede hacer de esta forma.
     while( sexo != "f" && sexo != "m") {
         sexo = prompt("Error. Ingrese f ó m .");
     }
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,22 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+	var contador = 0;
+	var acumulador = 0;
+	var num1;
+	var num2;
+	var num3;
+	var num4;
+	var num5;
+
+	while( contador = 0 ){
+
+		contador = parseInt(prompt("Ingrese sus números"));
+	}
+	
 
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById("suma").value = acumulador;
+document.getElementById("promedio").value = acumulador/5;
 
 }//FIN DE LA FUNCIÓN
