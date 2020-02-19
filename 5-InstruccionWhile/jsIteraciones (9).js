877bdfefd3cd10a3974e0bb1cb28a30b9ff15b58
alebianchini <alejocapo20@gmail.com>
2020-02-19 20:42:23 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -4,28 +4,37 @@ function mostrar()
 	var contador=0;
 	var numero;
 	var seguir;
+	var maximo;
+	var minimo;
 	
 
 	do{
-		numero = parseInt(prompt("Ingrese un número positivo o negativo"));
+		numero = parseInt(prompt("Ingrese un número: "));
 
 		while( isNaN(numero)){
-			numero = parseInt(prompt("Error. Ingrese un número positivo o negativo"));
+			numero = parseInt(prompt("Error. Ingrese un número válido: "));
 		}
 
 		
+		if( contador == 0 || maximo < numero){
+			
+			maximo = numero;
+			contador ++;
+		}
+		
+		if( contador == 1 || minimo > numero){
 
-
-
-
-
-
+			minimo = numero;
+		}
 
 
 		seguir = prompt("Quiere agregar otro número?");
 
 	} while( seguir == "s");
 
+
+document.getElementById("maximo").value = maximo;
+document.getElementById("minimo").value = minimo;
 
 
 }//FIN DE LA FUNCIÓN
