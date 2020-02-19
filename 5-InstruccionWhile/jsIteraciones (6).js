ff3411d2b0b15aaaba0103f085b2f22d753ad08c
alebianchini <alejobianchini@hotmail.com>
2020-02-19 01:15:21 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,22 +1,19 @@
 function mostrar()
 {
 
-	var contador = 0;
+	var numero;
 	var acumulador = 0;
-	var num1;
-	var num2;
-	var num3;
-	var num4;
-	var num5;
-
-	while( contador = 0 ){
-
-		contador = parseInt(prompt("Ingrese sus números"));
+	var contador = 0;
+//cuando una variable acumula en valores constantes ( variable + 1 + 1 + 1), hace de CONTADOR.
+//Cuando lo hace en valores variables ( variable + X + X + X ), hace de ACUMULADOR. --> X= numero.
+	while( contador < 5 ){
+		numero = parseInt(prompt("Ingrese un número"));
+		acumulador = acumulador + numero;
+		contador ++;
 	}
 	
-
-
-document.getElementById("suma").value = acumulador;
-document.getElementById("promedio").value = acumulador/5;
-
+	
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
+	
 }//FIN DE LA FUNCIÓN
